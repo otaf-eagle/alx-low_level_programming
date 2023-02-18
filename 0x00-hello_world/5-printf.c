d296f0b2c3a67ef1fc503eb81f5cb971837826cc
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-18 13:17:09 -0800

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - take void 
- * the printf function will be then printing out the argument 
- * return 0
- */
+* main - take void 
+* the printf function will be then printing out the argument 
+* return 0
+*/
 int main(void)
 {
-	printf("%s" , "with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
