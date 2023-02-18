06456759037b775e3f7b07e0969bcc791fa31057
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-18 03:27:47 -0800

    vo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-puts(/*"Programming is like building a multilingual puzzle*/)
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
