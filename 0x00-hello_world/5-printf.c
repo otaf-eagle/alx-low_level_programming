4eaae31d1fcbf3ae6cdf431a25dd5525405f811f
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-18 12:39:55 -0800

    t5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main function will take void argument , using the standard input output 
+ * the printf function will be then printing out the string , with proper grammar, but the outcome is a piece of art,
+ * finally returning a 0
+ */
+int main(void)
+{
+	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+		return 0;
+}
