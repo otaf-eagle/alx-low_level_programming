dc81e0b837164573321f6796aca2e6a0bbc4eff7
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-19 23:48:41 -0800

    lol

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	quote = char "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	fprintf(stderr, "%s", "quote\n");
 	return (1);
-}	
+}
