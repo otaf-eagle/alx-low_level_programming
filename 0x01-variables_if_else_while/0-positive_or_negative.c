86ba4f99bc1fedd7b9086ba5dde51f97bcd0415b
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-20 07:32:57 -0800

    olo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n==0);
+	if (n==0)
 	{
 		printf("%d : is zero", n );
 	}
-	else if (n>0);
+	else if (n>0)
 	{
 		printf("%d : is positive", n);
 	}
-	else;
+	else
 	{
 		printf("%d : is negative", n);
 	}
