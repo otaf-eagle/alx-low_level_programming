206748f77f228a58c45e6c0632d440b868f4b246
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-19 23:13:45 -0800

    t6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,4 +5,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{
+	char char_size;
+	int int_size;
+	long int long_int_size;
+	long long int l_l_int_size;
+	float float_size;
+	printf("size of char : %zu bytes\n", sizeof(char_size));
+	printf("size of int : %zu bytes\n", sizeof(int_size));
+	printf("size of long int : %zu bytes\n", sizeof(long_int_size));
+	printf("size of long long int : %zu bytes\n", sizeof(l_l_int_size));
+	printf("size of float : %zu bytes\n", sizeof(float_size));
 
+	return 0;
+}
