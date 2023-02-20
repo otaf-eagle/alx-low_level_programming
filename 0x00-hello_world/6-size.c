4987cf98cd66b1a8349abbcf8acce36e11d411ad
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-19 23:22:23 -0800

    go

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int l_l_int_size;
 	float float_size;
 
-	printf("size of char: %zu byte(s)\n", sizeof(char_size));
-	printf("size of int: %zu byte(s)\n", sizeof(int_size));
-	printf("size of long int: %zu byte(s)\n", sizeof(long_int_size));
-	printf("size of long long int: %zu byte(s)\n", sizeof(l_l_int_size));
-	printf("size of float: %zu byte(s)\n", sizeof(float_size));
+	printf("size of a char: %zu byte(s)\n", sizeof(char_size));
+	printf("size of an int: %zu byte(s)\n", sizeof(int_size));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long_int_size));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(l_l_int_size));
+	printf("size of a float: %zu byte(s)\n", sizeof(float_size));
 
 	return (0);
 }
