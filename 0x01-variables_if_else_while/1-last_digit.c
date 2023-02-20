656240b3913370de3bc0b652bba7e1781098ebd0
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-20 08:08:49 -0800

    lol

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,8 +20,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n%10);
 	}
-	else if (n%10 < 6 & !0)
+	else if (n%10 < (6 & !0))
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, %n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, %n10);
+	}
 	return (0);
 }
