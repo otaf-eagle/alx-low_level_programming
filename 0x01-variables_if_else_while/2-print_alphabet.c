328642331032bbc27bc4a5bf8c50abb48f92f466
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-22 02:32:57 -0800

    lol

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,10 @@
 int main(void)	
 {
 	int x
-	
-        for (x = 'A'; x <= 'Z'; x++)
-        {
-	        putchar(tolower(x));
-        }
+                for (x = 'A'; x <= 'Z'; x++)
+                {
+	                putchar(tolower(x));
+                }
         putchar('\n');
         return (0);
 }
