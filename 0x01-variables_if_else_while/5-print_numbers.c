0a654ab9a08d2c4d8b4bfb1d528fb5ba4fe31053
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-22 04:26:04 -0800

    lol

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	for (x = 0; x <= 9; x++)
+	{
+		printf(x);
+	}
+	putchar('\n');
+	return (0);
+}
