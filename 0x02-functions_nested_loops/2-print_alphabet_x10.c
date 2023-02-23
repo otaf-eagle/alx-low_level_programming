60dc68be492562e3108d92d74ac984adfd228557
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-23 08:45:22 -0800

    lol

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "1-alphabet.c"
+
 /**
  * print_alphabet_x10 - prints 10 times each alphabet
  */
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether a letter is lower case
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
