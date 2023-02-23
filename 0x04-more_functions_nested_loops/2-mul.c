b03e75e9767ddf573e6053220da93ea0141b70b9
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-23 10:43:28 -0800

    lol

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of multuiplication
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = (a * b);
+	return (c);
+}
