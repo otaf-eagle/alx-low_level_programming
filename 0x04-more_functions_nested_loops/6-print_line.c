93ba5f58901a07bf2121282630902c3d96bcfea2
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-23 13:07:13 -0800

    lol

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times _ will be printed
+ * Return: success line. otherwise new line
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
