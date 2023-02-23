c5a99303e8701fb01181732540ef80080f376038
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-23 08:52:04 -0800

    lol

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * _islower - checks whether a letter is lower case
  * @c: character
  *
- * Return: always 1 on success . 0 otherwise  
+ * Return: always 1 on success . 0 otherwise
  */
 int _islower(int c)
 {
