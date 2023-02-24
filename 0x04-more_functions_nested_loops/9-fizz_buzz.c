5ecc4cc7dab3e9747b3ffd163902790839fa9157
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-02-24 04:22:42 -0800

    lol

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,38 +2,43 @@
 #include <stdio.h>
 
 /**
- * Entry - fizz buzz multiples of 3 and 5
- *
- * Return: fizz,buzz,fizzbuzz
+ * Entry: FizzBuzz game
+ * chaecks for multiples of 3 and 5 and combined
+ * Return: printed array
  */
 int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
-      	 {
-             	 if (i % 15 == 0)
-              	 {
-             	        printf("FizzBuzz");
-              	        putchar(' ');
-             	  }
-              	else if (i % 5 == 0)
-             	 {
-              	        printf("Buzz");
-              	        if (i == 100)
-              	        {
-               	        }
-              	        else
-              	                putchar(' ');
-              	 }
-       	        else if (i % 3 == 0)
-       	        {
-       	                printf("Fizz");
-       	                putchar(' ');
-      	        }
-       	        else
-      	              printf("%d ", i);
-       	}
-	putchar('\n');
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			if(i == 100)
+			{
+			}
+			else
+			{
+				putchar(' ');
+			}
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d", i);
+			putchar(' ');
+		}
+		putchar('\n');
+	}
 	return (0);
 }
