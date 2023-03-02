1f344c88d8937c4ac99df4709e57b0e63d78f307
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-03-02 07:29:11 -0800

    lol

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,14 @@
 /**
  * _strlen - prints length of string
  * @s: string
- * return: length of string on success
+ * Return: length of string on success
  */
 int _strlen(char *s)
 {
 	int c;
 
 	c = 0;
-	while(*(s + c) != '\0')
+	while (*(s + c) != '\0')
 	{
 		c = c + 1;
 	}
