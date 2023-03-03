7943463dd7acbfb023cec91f076e2dbbfb771650
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-03-02 16:12:52 -0800

    lol

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - appends n numbers from s2 to s1
+ * @dest: string 1
+ * @src: string 2
+ * Return: appended string 1
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, len;
+	
+	len = strlen(src);
+	i = 0;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	if (len < n)
+	{
+		while (len != j)
+		{
+			*(dest + i) = *(src + j);
+			j++;
+			i++;
+		}
+	}
+	else
+	{
+		while (*(src + j) != '\0')
+		{
+			if (j == n)
+				break;
+			*(dest + i) = *(src + j);
+			j++;
+			i++;
+		}
+	}
+	return (dest);
+}
