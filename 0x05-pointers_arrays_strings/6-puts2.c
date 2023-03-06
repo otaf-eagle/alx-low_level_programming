0a6646cdc038c2a5011e791472c84266970d9413
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-03-06 14:57:25 -0800

    lol

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,7 +14,7 @@ void puts2(char *str)
 	len = strlen(str);
 	for (i = 0; i != len; i = i + 2)
 	{
-		_putchar(str[i]);
+		_putchar(*(str + i));
 	}
 	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string value
+ * Return: on success always zero
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len;
+	int n;
+	int x;
+	int y;
+
+	len = strlen(str);
+	n = (len - 1) / 2;
+	if (n %2 == 0)
+	{
+		x = n / 2;
+		for(i = x; i <= len; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+	else if (n %2 != 0)
+	{
+		y = n / 2;
+		for(i = y; i <= len; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+}
