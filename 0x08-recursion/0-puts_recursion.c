7e42b15adb8c5ce7afb2f1bad771bd17f9f1ef1e
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-03-10 06:42:50 -0800

    lol

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,18 +7,14 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	if (*(s + i) == '\0')
+	if (*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion((s + 1));
+	}
+	else
 	{
 		putchar('\n');
 		return;
 	}
-	else
-	{
-
-		putchar(s[i]);
-	}
-	i++;
 }
