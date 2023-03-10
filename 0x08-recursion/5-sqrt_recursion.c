e57f8f29d962544fe80d19f14d6c06916876c03f
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-03-10 08:21:14 -0800

    lol

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - gets a square root of a number
+ * @n: number to be evaluated
+ * Return: the natural square root if n fail -1
+ */
+int _sqrt_recursion(int n)
+{
+
