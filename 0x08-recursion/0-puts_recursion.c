7c1d01648073ead0bc948fe5c2ffdf357d80e7b8
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-03-10 06:07:53 -0800

    lol

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by n
+ * @s: string to be printed
+ * Return: on success 0 and the printed s
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	i++;
+	if (*(s + i) == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*(s + i));
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+
+int _putchar(char x)
+{
+	putchar(x);
+	return(0);
+}
