b6130fd28dc5bee7b3d0af47469183c9e5619eed
otaf-eagle <122812843+otaf-eagle@users.noreply.github.com>
2023-03-12 13:58:07 -0700

    lol

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints number of arguments
+ * @argc: arguments
+ * @argv: vector argument array
+ * Return: zero on success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n",argc);
+	return (0);
+}
